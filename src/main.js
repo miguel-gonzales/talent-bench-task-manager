b11da8807cb3c161f42502ef6ed2f9ef8387a77e
Miguel Gonzales <miguel.gonzales@assuresoft.com>
2025-09-03 18:51:14 -0400

    fix: update import pointing to wrong directory.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
-import { TaskRepository } from "./infrastructure/TaskRepository.js";
+import { TaskRepository } from "./repository/TaskRepository.js";
+import { TaskController } from "./interface/TaskController.js";
 import { TaskView } from "./interface/TaskView.js";
 
 
 const repo = new TaskRepository();
 const view = new TaskView();
+const controller = new TaskController(repo, view);
 controller.init();
